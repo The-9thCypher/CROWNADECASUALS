1fc63c8807977289a452a1e22256744eeb5cb5b2
Shyllon Olayinka <shyllonolayinka608@gmail.com>
2022-08-31 06:51:33 +0100

    js

--- crownade.js
+++ crownade.js
@@ -2,16 +2,3 @@ function myFunc(){
     alert("Please wait, this should take less than a minute");
   } 
 
-// const btn=document.getElementById("btn");
-
-// btn.addEventListener("click", function myFunc(){
-//     alert("Please wait, this should take less than a minute")
-// });
-
-
-// function clickHandler(event){
-//     console.log('Button clicked');
-// }
-
-// const btn = document.querySelector('.btn');
-// btn.addEventListener('click', clickHandler)
