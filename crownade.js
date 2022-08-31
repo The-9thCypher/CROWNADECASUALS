3a3aafce4e405fd2fbf43586b391b02a35ff00da
Shyllon Olayinka <shyllonolayinka608@gmail.com>
2022-08-31 07:13:26 +0100

    animations

--- crownade.js
+++ crownade.js
@@ -2,3 +2,12 @@ function myFunc(){
     alert("Please wait, this should take less than a minute");
   } 
 
+var express= require("express");
+var app= express();
+
+
+  var server = app.listen(8000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Example app listening at https://%s:%s", host, port);
+  });
