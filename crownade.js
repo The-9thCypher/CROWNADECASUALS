d75429ff058c53ad74f2d374d5bb73386b84f480
Shyllon Olayinka <shyllonolayinka608@gmail.com>
2022-09-01 12:41:45 +0100

    js

--- crownade.js
+++ crownade.js
@@ -3,13 +3,28 @@ function email(){
   +"?subject=" + encodeURIComponent("Order to Crownade Casuals")
   ;
   window.location.href = link;
+  window.alert("Sending email");
   } 
 
-var express= require("express");
-var app= express();
+  // var timeout = setTimeout(function(){
+  //   window.alert("You have spent 5 seconds on this website");
+  // }, 5000);
 
-  var server = app.listen(8000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Example app listening at https://%s:%s", host, port);
-  });
+// function Validateemail() {
+//   var email = document.getElementsById("email-input").value;
+//   if(email == "") {
+//     alert("Input a valid email");
+//     return false;
+//   }
+// }
+
+
+// var express= require("express");
+// var app= express();
+
+//   var server = app.listen(8000, function () {
+//     var host = server.address().address;
+//     var port = server.address().port;
+//     console.log("Example app listening at https://%s:%s", host, port);
+//   });
+
